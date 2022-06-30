80890db55e8476d9fe078882bfed2482e60004ec
beauter <marajibeauter0@gmail.com>
2022-06-30 14:14:50 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-
-    puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
+int main(void)
+{
+    char str[]="\"Programming is like building a multilingual puzzle";
+    puts(str);
+    return 0;
 }
