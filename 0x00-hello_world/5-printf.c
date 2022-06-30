db9f4e3870c746079327651c9c7a3b80c9c38ef3
beauter <marajibeauter0@gmail.com>
2022-06-30 15:05:10 +0300

    printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 /**
- *main - print a string
- *Return: puts string
- */
+ **main - Main function
+ **Return: return 0 everytime
+ **/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
