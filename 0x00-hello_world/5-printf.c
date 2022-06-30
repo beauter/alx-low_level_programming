472024edf7efba4324f7a940d32cbeaa8daca34a
beauter <marajibeauter0@gmail.com>
2022-06-30 15:22:11 +0300

    print f file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+
 /**
- **main - Main function
- **Return: return 0 everytime
- **/
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     **/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
