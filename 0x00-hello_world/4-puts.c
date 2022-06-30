bdb6e8e764a4f0122b1d83fbeb534c3c68300101
beauter <marajibeauter0@gmail.com>
2022-06-30 13:50:46 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+
 /**
- * main - Prints "Programming is like building a
- *   multilingual puzzle"
- *
- * Return: Always 0 (Success)
- */
+ **main - Main function of my project
+ **Return: return 0 everytime
+ **/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	char show[52];
+
+	strcpy(show, "\"Programming is like building a multilingual puzzle");
+	puts(show);
+	return (0);
 }
