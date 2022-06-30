cf8a0ad7f4ad13b5429775af94ad042736cf183c
beauter <marajibeauter0@gmail.com>
2022-06-30 13:37:51 +0300

    c programing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <studio.h>
-int main()
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: no arguments
+ * Description: the program starts and ends here.
+ * Return: value 0 to exit
+ */
+int main(void)
 {
-    puts("\"programing is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
