097da97c55579ff6f5a23ac5111a5abb60758112
beauter <marajibeauter0@gmail.com>
2022-06-30 15:03:01 +0300

    print if

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - print a string
+ *Return: puts string
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
