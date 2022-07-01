9fe92fa6ca40240a46ae6b40dcbe3ebc8698b843
beauter <marajibeauter0@gmail.com>
2022-07-01 12:27:42 +0000

    last digits correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,31 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- *  * main - this is the only function
- *   * Description: It will print the last digit of the number stored in n
- *    * Return: The function returns 0
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n, lastDig;
+	int n;
+	int l;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDig = n % 10;
+	l = n % 10;
 
-	if (lastDig > 5)
+	if (l > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (lastDig == 0)
+	else if (l == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDig);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else if (lastDig < 6 && !lastDig == 0)
+	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
