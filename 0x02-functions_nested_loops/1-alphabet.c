974de33f981b0be07d1e70ea2071b5a1edb328de
beauter <marajibeauter0@gmail.com>
2022-07-05 12:08:26 +0000

    lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main Entry point
+ * prints lowercase alphabets
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char q = 'a'
+	while (q <= 'z')
+	{
+		_putchar(q);
+		q++;
+	}
+	_putchar('\n');
+}
