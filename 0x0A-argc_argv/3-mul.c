ceaf2e84d41be32fd74270d54cfd0016e6799780
beauter <marajibeauter0@gmail.com>
2022-07-22 14:14:11 +0000

    corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Mian
  * @argc: int type
@@ -8,11 +9,21 @@
  **/
 int main(int argc, char *argv[])
 {
-int count;
+int x = 0, y = 0;
 
-for (count = 0; count < argc; count++)
+if (argc == 3)
 {
-printf("%s\n", argv[count]);
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+
+printf("%d\n", x *y);
+
+}
+
+else
+{
+printf("Error\n");
+return (1);
 }
 return (0);
 }
