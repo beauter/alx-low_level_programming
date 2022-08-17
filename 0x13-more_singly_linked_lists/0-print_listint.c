c66cfd8020ef5b49209cac90bf66858d7c6311ce
beauter <marajibeauter0@gmail.com>
2022-08-17 13:10:25 +0000

    tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,12 +4,13 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the ltint_t *h)
-{
+ */
+ {
 	size_t nodes = 0;
 	while (h)
 	{
 		nist_t list.
- *
+/**
  * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const lisodes++;
